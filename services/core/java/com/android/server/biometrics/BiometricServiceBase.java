30b2ea159293c553f97d39869954394d8b985387
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2020-03-22 19:21:06 +0200

    Update BiometricServiceBase.java

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -841,7 +841,11 @@ public abstract class BiometricServiceBase extends SystemService
             ClientMonitor client = mCurrentClient;
             if (client instanceof EnrollClient && client.getToken() == token) {
                 if (DEBUG) Slog.v(getTag(), "Cancelling enrollment");
-                client.stop(client.getToken() == token);
+                final int stopResult = client.stop(true);
+                if (mNotifyClient && (stopResult == 0)) {
+                    handleError(mHalDeviceId,
+                            BiometricConstants.BIOMETRIC_ERROR_CANCELED, 0);
+                }
             }
         });
     }
